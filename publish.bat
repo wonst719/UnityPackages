0a4498ec3d80514ad332ae94d87b7e8f406efced
wonst719 <wonst719@gmail.com>
2020-01-30 16:26:14 +0900

    Initial Commit - Extenject - MessagePack - MasterMemory - LZ4

---
+++ publish.bat
@@ -0,0 +1,23 @@
+@rem call npm unpublish com.star.neuecc.messagepack@2.0.335 --force --registry http://localhost:4873
+
+@rem call npm unpublish com.star.tasharen.ngui.examples --force --registry http://localhost:4873
+
+@rem call npm publish System.Buffers --registry http://localhost:4873
+@rem call npm publish System.Runtime.CompilerServices.Unsafe --registry http://localhost:4873
+@rem call npm publish System.Memory --registry http://localhost:4873
+@rem call npm publish System.Threading.Tasks.Extensions --registry http://localhost:4873
+
+@rem call npm publish MessagePack --registry http://localhost:4873
+@rem call npm publish MasterMemory --registry http://localhost:4873
+
+@rem call npm publish Extenject --registry http://localhost:4873
+@rem call npm publish NGUI --registry http://localhost:4873
+@rem call npm publish NGUI.Examples --registry http://localhost:4873
+
+@rem call npm publish K4os.Compression.LZ4 --registry http://localhost:4873
+@rem call npm publish K4os.Compression.LZ4.Legacy --registry http://localhost:4873
+
+@rem call npm unpublish com.star.tasharen.ngui --force --registry http://localhost:4873
+@rem call npm publish NGUI --registry http://localhost:4873
+
+pause
